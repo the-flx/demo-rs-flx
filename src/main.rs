6da36a5608310b0c24af104240cb131ca1955410
JenChieh <jcs090218@gmail.com>
2025-12-18 22:30:31 +0800

    done

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
-include!("./flx.rs");
-
-use std::ffi::CString;
-
 fn main() {
-    let str = CString::new("buffer-file-name").expect("CString::new failed");
-    let query = CString::new("bfn").expect("CString::new failed");
+    let result = rs_flx::score("buffer-file-name", "bfn");
 
-    unsafe {
-        let result = flx_score(str.as_ptr(), query.as_ptr());
-
-        println!("Hello, world!: {:p}", result);
-    }
+    println!("Hello, world!: {}", result.score);
 }
